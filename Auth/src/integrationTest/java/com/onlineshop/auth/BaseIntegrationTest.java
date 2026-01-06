74059109c15ea712346ec348c2a69bf9cdcec4da
DamyanManev <damyanmanev@gmail.com>
2026-01-06 09:23:49 +0200

    Integration tests issue with docker fixed

--- Auth/src/integrationTest/java/com/onlineshop/auth/BaseIntegrationTest.java
+++ Auth/src/integrationTest/java/com/onlineshop/auth/BaseIntegrationTest.java
@@ -5,27 +5,13 @@ import com.onlineshop.auth.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.postgresql.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
+@Import(TestcontainersConfiguration.class)
 @ActiveProfiles("integration-test")
-// @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class BaseIntegrationTest {
-
-    @Container
-    @ServiceConnection
-    @SuppressWarnings("resource")
-    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18-alpine")
-            .withDatabaseName("auth_test")
-            .withUsername("test")
-            .withPassword("test")
-            .withInitScript("init-db/01-schema.sql");
 
     @Autowired
     protected UserRepository userRepository;
