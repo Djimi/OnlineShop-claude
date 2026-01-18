cf86dee65a7f1efa95a4845fcf8b7fda906dda0d
DamyanManev <damyanmanev@gmail.com>
2026-01-18 18:51:06 +0200

    Redundant code removed

--- api-gateway/src/main/java/com/onlineshop/gateway/dto/ValidateResponse.java
+++ api-gateway/src/main/java/com/onlineshop/gateway/dto/ValidateResponse.java
@@ -27,9 +27,4 @@ public class ValidateResponse implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     private Instant expiresAt;
 
-    public static ValidateResponse invalid() {
-        return ValidateResponse.builder()
-                .valid(false)
-                .build();
-    }
 }
